d72575354f40631b570ccb66eab7476cad3d7f0b
koshulia <98849984+koshulia@users.noreply.github.com>
2022-11-02 16:53:07 +0200

    commit

--- lesson13-modules-and-unit-tests/finding-the-smallest-square/getMinSquaredNumber.js
+++ lesson13-modules-and-unit-tests/finding-the-smallest-square/getMinSquaredNumber.js
@@ -1,4 +1,5 @@
 export default arr => {
+  if (!Array.isArray(arr) || arr === []) return null;
   const res = arr.map(num => Math.abs(num * num));
   return Math.min.apply(Math, res);
 };
--- lesson13-modules-and-unit-tests/finding-the-smallest-square/getMinSquaredNumber.test.js
+++ lesson13-modules-and-unit-tests/finding-the-smallest-square/getMinSquaredNumber.test.js
@@ -4,3 +4,13 @@ it('should get min squared number', () => {
   const result = getMinSquaredNumber([4, 0.6, -8]);
   expect(result).toEqual(0.36);
 });
+
+it('should not be empty array', () => {
+  const result = getMinSquaredNumber([]);
+  expect(result).not.toEqual([]);
+});
+
+it('should be an array', () => {
+  const result = getMinSquaredNumber(6, 4, 7);
+  expect(result).not.toEqual(16);
+});
