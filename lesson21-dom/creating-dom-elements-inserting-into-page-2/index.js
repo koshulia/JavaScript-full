8942064ebdf7a0b6380443dfc4f58d11d4c9e547
koshulia <98849984+koshulia@users.noreply.github.com>
2022-11-22 01:29:51 +0200

    created function that finishes list

---
+++ lesson21-dom/creating-dom-elements-inserting-into-page-2/index.js
@@ -0,0 +1,15 @@
+export function finishList() {
+  const listElem = document.querySelector('.list');
+  const specialElem = document.querySelector('.special');
+  const firstItem = document.createElement('li');
+  firstItem.textContent = 1;
+  const fourthItem = document.createElement('li');
+  fourthItem.textContent = 4;
+  const sixthItem = document.createElement('li');
+  sixthItem.textContent = 6;
+  listElem.prepend(firstItem);
+  specialElem.before(fourthItem);
+  specialElem.after(sixthItem);
+}
+
+finishList();
