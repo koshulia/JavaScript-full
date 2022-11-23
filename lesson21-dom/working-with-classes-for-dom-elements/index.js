84a4003695cfb50fcfbee1d85277979d01ea32fb
koshulia <98849984+koshulia@users.noreply.github.com>
2022-11-23 04:03:49 +0200

    created function that changes class

---
+++ lesson21-dom/working-with-classes-for-dom-elements/index.js
@@ -0,0 +1,13 @@
+export function manageClasses() {
+  const one = document.querySelector('.one');
+  const two = document.querySelector('.two');
+  const three = document.querySelector('.three');
+  const four = document.querySelector('.four');
+
+  one.classList.add('selected');
+  two.classList.remove('selected');
+  three.classList.toggle('three_done');
+  if (four.classList.contains('some-class')) four.classList.add('another-class');
+}
+
+manageClasses();
