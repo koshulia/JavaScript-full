02922bfc6c742d6792492e3086a0d47c7111ab25
koshulia <98849984+koshulia@users.noreply.github.com>
2022-11-25 14:30:21 +0200

    updated

--- lesson22-events-browser-handling-bubbling/handle-event-for-checkbox/index.js
+++ lesson22-events-browser-handling-bubbling/handle-event-for-checkbox/index.js
@@ -2,6 +2,7 @@ const checkboxElem = document.querySelector('.task-status');
 
 const getCheckbox = () => {
   console.log(checkboxElem.checked);
+  console.log(checkboxElem.мфдгу);
 };
 
 checkboxElem.addEventListener('click', getCheckbox);
