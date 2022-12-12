bbf87524b51d372688d2effac41f36c98547e7e2
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-12 17:25:45 +0200

    created a funk that finds day of week in certain quantity of days

--- lesson24-working-with-dates/installing-date-components/index.js
+++ lesson24-working-with-dates/installing-date-components/index.js
@@ -1,3 +1,12 @@
-export const dayOfWeek = (date, days){
-    
-} 
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+export const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+
+const result = dayOfWeek(new Date(2022, 11, 12), 8);
+
+console.log(result);
