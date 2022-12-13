f84b6b37d15316de32c30be15151b0ae00977e96
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-13 17:59:50 +0200

    added condition

--- lesson24-working-with-dates/date-difference/index.js
+++ lesson24-working-with-dates/date-difference/index.js
@@ -1,12 +1,11 @@
 export const getDiff = (startDate, endDate) => {
-  if (startDate > endDate) return;
   const res = new Date(startDate) - new Date(endDate);
   const days = Math.round(res / (1000 * 3600 * 24));
   const hours = new Date(res).getUTCHours();
   const minutes = new Date(res).getMinutes();
   const seconds = new Date(res).getSeconds();
 
-  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  return `${Math.abs(days)}d ${hours}h ${minutes}m ${seconds}s`;
 };
 
 const result = getDiff(new Date(2022, 11, 12, 16, 45, 18), new Date(2019, 4, 5, 10, 30, 56));
