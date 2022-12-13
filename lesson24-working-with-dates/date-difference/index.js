85047cad0d1597afdf7594629d6f65e82edde1c2
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-13 17:47:23 +0200

    finished func that shows difference between two dates

--- lesson24-working-with-dates/date-difference/index.js
+++ lesson24-working-with-dates/date-difference/index.js
@@ -1,16 +1,16 @@
 export const getDiff = (startDate, endDate) => {
   const res = new Date(startDate) - new Date(endDate);
-  const days = Math.ceil(res / (1000 * 3600 * 24));
-  const hours = Math.ceil(res / days / 3600);
-  const minutes = hours / 60;
-  const seconds = minutes % 1000;
-  //   console.log(res);
-  console.log(days);
-  console.log(hours);
-  //   console.log(minutes);
+  const days = Math.round(res / (1000 * 3600 * 24));
+  const hours = new Date(res).getUTCHours();
+  const minutes = new Date(res).getMinutes();
+  const seconds = new Date(res).getSeconds();
+
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 };
 
-const result = getDiff(new Date(2022, 11, 12, 16, 45, 15), new Date(2019, 4, 5, 13, 30, 56));
+const result = getDiff(new Date(2022, 11, 12, 16, 45, 18), new Date(2019, 4, 5, 10, 30, 56));
+
+const result1 = getDiff(new Date(2002, 11, 12, 16, 40, 15), new Date(2019, 4, 5, 6, 30, 56));
 
 console.log(result);
+console.log(result1);
