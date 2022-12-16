68cbcd33cf6b4f5597b3ebc0663c507003aae500
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-16 04:19:45 +0200

    added JSON.parse

--- lesson25-browser-local-storage/getting-all-localstorage-data/index.js
+++ lesson25-browser-local-storage/getting-all-localstorage-data/index.js
@@ -2,12 +2,13 @@ localStorage.clear();
 localStorage.setItem('name', JSON.stringify({ name: 'Tom' }));
 localStorage.setItem('age', JSON.stringify({ age: 17 }));
 localStorage.setItem('hobbies', JSON.stringify({ hobbies: ['skiing', 'paragliding'] }));
+localStorage.setItem('male', JSON.stringify({ male: true }));
 
 export const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
     return {
       ...acc,
-      [key]: value,
+      [key]: JSON.parse(value),
     };
   }, {});
 };
