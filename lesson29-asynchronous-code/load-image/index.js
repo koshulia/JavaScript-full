ae679b74e70f46e163de60d101c0825d8b653f07
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-24 04:32:08 +0200

    changed alt

--- lesson29-asynchronous-code/load-image/index.js
+++ lesson29-asynchronous-code/load-image/index.js
@@ -1,6 +1,6 @@
 export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'Nature');
+  imgElem.setAttribute('alt', 'My Photo');
   imgElem.src = imgSrc;
   const containerElem = document.querySelector('.page');
   if (!imgElem) return;
