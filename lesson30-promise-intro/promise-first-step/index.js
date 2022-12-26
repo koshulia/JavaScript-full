0d6f6ea8a9205346e80f0c412ada0c5191943c5f
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-26 21:19:55 +0200

    created func that uses promicse

---
+++ lesson30-promise-intro/promise-first-step/index.js
@@ -0,0 +1,27 @@
+export const addImage = imgSrc => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    if (!containerElem) return;
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+
+  return p;
+};
+
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const result = addImage(imgSrc);
+
+console.log(result);
