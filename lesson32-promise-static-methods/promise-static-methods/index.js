ad43aa5a0e02f961e67a0398ec865e31f3742d2a
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-31 03:26:39 +0200

    created func that works with async numbers

---
+++ lesson32-promise-static-methods/promise-static-methods/index.js
@@ -0,0 +1,25 @@
+const getValueWithDelay = (value, delay) =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      console.log(value);
+      resolve(value);
+    }, delay);
+  });
+
+const asyncNumber1 = getValueWithDelay(56, 1000);
+const asyncNumber2 = getValueWithDelay(70, 2000);
+const asyncNumber3 = getValueWithDelay(139, 3000);
+const asyncNumber4 = getValueWithDelay(undefined, 3000);
+const asyncNumber5 = getValueWithDelay('jou', 3000);
+
+export const getSum = numbers =>
+  numbers.filter(value => !isNaN(value)).reduce((num, acc) => acc + Number(num), 0);
+
+const asyncSum = (...asyncNumbers) =>
+  Promise.all(asyncNumbers)
+    .then(numbers => getSum(numbers))
+    .catch(() => Promise.reject(new Error('Can`t calculate')));
+
+asyncSum(asyncNumber1, asyncNumber2, asyncNumber3, asyncNumber4, asyncNumber5).then(result =>
+  console.log(result),
+);
