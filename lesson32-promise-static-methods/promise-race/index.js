9c1e07bc23f89d35e24f6cbebae6a46a63c06b51
Uliana Koshuba <koshubaulyana@gmail.com>
2022-12-31 04:07:54 +0200

    added resolve of fastest promise

--- lesson32-promise-static-methods/promise-race/index.js
+++ lesson32-promise-static-methods/promise-race/index.js
@@ -18,7 +18,10 @@ const servers = ['https://server.com/us', 'https://server.com/eu', 'https://serv
 export const getUserASAP = userId => {
   const userUrls = servers.map(serverUrl => `${serverUrl}/users/${userId}`);
   const requests = userUrls.map(userUrl => request(userUrl));
+
   return Promise.race(requests);
 };
 
-getUserASAP('user-id-1').then(res => console.log(res));
+getUserASAP('user-id-1')
+  .then(res => console.log(res))
+  .then(res => Promise.resolve(res));
