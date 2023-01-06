efca3a56cecd42a40f9143d49b66e3cb8cee691f
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-06 20:57:44 +0200

    update

--- lesson35-error-handling/handling-errors-in-promise/index.js
+++ lesson35-error-handling/handling-errors-in-promise/index.js
@@ -2,7 +2,7 @@ const successRequest = Promise.resolve({ name: 'Tom' });
 
 successRequest
   .then(function onSuccess1(data) {
-    console.log(data);
+    // console.log(data);
     throw new Error('Error with data');
   })
   .catch(function onError1(error) {
@@ -14,7 +14,7 @@ const failRequest = Promise.reject(new Error('Something went wrong'));
 failRequest
   .catch(function onError2(error) {
     console.error('onError2', error.message);
-    throw new Error('Server error');
+    return Promise.reject(new Error('Server error'));
   })
   .then(function onSuccess2(data) {
     console.log('onSuccess2', data);
